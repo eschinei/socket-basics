827af440b7dd04a9ea7b33658cb48f37bfc34dd3
Ismael Chinea <eschinei@gmail.com>
2016-05-12 13:50:54 +0200

    Added custom message event

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var PORT = process.env.PORT || 3000;
-var express = require ('express');
+var express = require('express');
 var app = express();
 
 // built-in node module http
@@ -8,12 +8,22 @@ var http = require('http').Server(app);
 
 var io = require('socket.io')(http);
 
-io.on ('connection', function (){
+io.on('connection', function(socket) {
 	console.log('User connected via socket.io');
+
+	socket.on('message', function(message){
+		console.log ('Message received: ' + message.text);
+		socket.broadcast.emit ('message', message);
+		// io.emit
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to the chat application'
+	});
 });
 
 app.use(express.static(__dirname + '/public'));
 
-http.listen(PORT, function (){
+http.listen(PORT, function() {
 	console.log('Server started at port: ' + PORT);
-})
+});
